edccabee0159448f27f9a2f14c08a50de84694cb
PhantomOrion2000 <88031868+PhantomOrion2000@users.noreply.github.com>
2022-08-17 09:10:17 +0530

    new-stackp-added

---
+++ Stack&Queue/Delete middle element from stack.cpp
@@ -0,0 +1,18 @@
+void solve(stack<int>&inputStack, int count, int size) {
+    if(count == size/2){
+        inputStack.pop();
+        return;
+    }
+    int temp = inputStack.top();
+    inputStack.pop();
+    solve(inputStack, count+1, size);
+    inputStack.push(temp);
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   // Write your code here
+    int count = 0;
+    solve(inputStack, count, N);
+   
+}
